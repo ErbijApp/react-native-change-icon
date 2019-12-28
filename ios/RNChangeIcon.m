ea5f1ed5c4c6f8c220b95ee0c75a8a27fca83b51
Michiel van Roon <michiel@michielvanroon.nl>
2019-12-28 17:32:29 +0100

    show dialog again

--- ios/RNChangeIcon.m
+++ ios/RNChangeIcon.m
@@ -27,18 +27,10 @@ RCT_REMAP_METHOD(changeIcon, iconName:(NSString *)iconName resolver:(RCTPromiseR
 
     resolve(@(YES));
 
-    NSMutableString *selectorString = [[NSMutableString alloc] initWithCapacity:40];
-    [selectorString appendString:@"_setAlternate"];
-    [selectorString appendString:@"IconName:"];
-    [selectorString appendString:@"completionHandler:"];
-
-    SEL selector = NSSelectorFromString(selectorString);
-    IMP imp = [[UIApplication sharedApplication] methodForSelector:selector];
-    void (*func)(id, SEL, id, id) = (void *)imp;
-    if (func)
-    {
-        func([UIApplication sharedApplication], selector, iconName, ^(NSError * _Nullable error) {});
-    }
+    // Custom icon
+    [[UIApplication sharedApplication] setAlternateIconName:iconName completionHandler:^(NSError * _Nullable error) {
+        RCTLog(@"%@", [error description]);
+    }];
 }
 
 @end
